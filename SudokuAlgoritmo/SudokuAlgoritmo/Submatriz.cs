f274883078d5877a0f2b7b3ca0113ae9d53ebc78
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-04-13 19:31:03 -0300

    Lista de números disponíveis em submatriz

--- SudokuAlgoritmo/SudokuAlgoritmo/Submatriz.cs
+++ SudokuAlgoritmo/SudokuAlgoritmo/Submatriz.cs
@@ -9,14 +9,24 @@ namespace SudokuAlgoritmo
     public class Submatriz
     {
         public string localizacao { get; set; }
-        public int[,] submatriz = new int[3,3];
+        public int[,] submatriz = new int[3, 3];
+        public List<int> numerosDisponiveis = new List<int>();
 
         public Submatriz(string local) { 
             this.localizacao = local;
+            for (int i = 0; i < 10; i++)
+            {
+                numerosDisponiveis.Add(i);
+            }
         }
 
         public string Display()
         {
+            for (int i = 0; i < numerosDisponiveis.Count; i++)
+            {
+                Console.WriteLine(numerosDisponiveis[i]);
+            }
+            
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
